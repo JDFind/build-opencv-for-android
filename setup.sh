546c9ea9aab4a3ea560dd1c365884c41c71c6f26
Andrei Nicolae <andreinicolae.gtr@gmail.com>
2017-08-28 01:04:52 +0300

    Add some comments.

--- setup.sh
+++ setup.sh
@@ -1,7 +1,13 @@
 # Modifications Copyright (c) 2017 JDFind
 # Original work Copyright (c) 2016 Tzutalin
 # For licensing, read LICENSE
+#
+# Script for downloading android-ndk-downloader, opencv, opencv contrib 
+# Requires git
+# Usage:
+#     $ ./setup.sh [-v <opencv_version>]
 
+# read Open Cv version
 OPENCV_VERSION=3.3.0
 while getopts "v:" opts; do
     case ${opts} in
@@ -10,9 +16,11 @@ while getopts "v:" opts; do
 done
 echo "Using Open CV version ${OPENCV_VERSION}"
 
+# setup path
 SCRIPT=$(readlink -f $0)
 WD=`dirname $SCRIPT`
-### Download android-ndk-downloader
+
+# clone android-ndk-downloader
 if [ ! -d "${WD}/android-ndk-downloader" ]; then
     echo 'Cloning android-ndk-downloader'
     git clone --recursive https://github.com/tzutalin/android-ndk-downloader.git
@@ -20,6 +28,7 @@ if [ ! -d "${WD}/android-ndk-downloader" ]; then
     python download_ndk.py
 fi
 
+# clone Open CV
 cd "${WD}"
 if [ ! -d "${WD}/opencv" ]; then
     echo 'Cloning opencv'
@@ -28,6 +37,7 @@ fi
 cd opencv
 git checkout -b "${OPENCV_VERSION}" "${OPENCV_VERSION}"
 
+# clone Open CV Contrib
 cd "${WD}"
 if [ ! -d "${WD}/opencv_contrib" ]; then
     echo 'Cloning opencv_contrib'
