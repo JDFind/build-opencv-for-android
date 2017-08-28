0be6baee59faca18cb09afa8798d27c90eaddbfd
Andrei Nicolae <andreinicolae.gtr@gmail.com>
2017-08-28 05:14:08 +0300

    Add more Cmake flags as stated in the docs. http://docs.opencv.org/3.1.0/d7/d9f/tutorial_linux_install.html

--- build-android-opencv.sh
+++ build-android-opencv.sh
@@ -30,19 +30,22 @@ do
     mkdir -p "${temp_build_dir}"
     cd "${temp_build_dir}"
 
-    cmake -DCMAKE_BUILD_WITH_INSTALL_RPATH=ON \
-          -DCMAKE_TOOLCHAIN_FILE="${WD}/android-cmake/android.toolchain.cmake" \
-          -DANDROID_NDK="${ANDROID_NDK_ROOT}" \
-          -DANDROID_NATIVE_API_LEVEL=${API_LEVEL} \
-          -DANDROID_ABI="${ANDROID_ABI}" \
+    cmake -D CMAKE_BUILD_TYPE=Release \
+		  -D CMAKE_BUILD_WITH_INSTALL_RPATH=ON \
+		  -D CMAKE_INSTALL_PREFIX="${INSTALL_DIR}/opencv" \
+          -D CMAKE_TOOLCHAIN_FILE="${WD}/android-cmake/android.toolchain.cmake" \
+          -D ANDROID_NDK="${ANDROID_NDK_ROOT}" \
+          -D ANDROID_NATIVE_API_LEVEL=${API_LEVEL} \
+          -D ANDROID_ABI="${ANDROID_ABI}" \
           -D WITH_CUDA=OFF \
           -D WITH_MATLAB=OFF \
           -D BUILD_ANDROID_EXAMPLES=OFF \
           -D BUILD_DOCS=OFF \
           -D BUILD_PERF_TESTS=OFF \
           -D BUILD_TESTS=OFF \
-          -DOPENCV_EXTRA_MODULES_PATH="${WD}/opencv_contrib/modules/"  \
-          -DCMAKE_INSTALL_PREFIX="${INSTALL_DIR}/opencv" \
+		  -D BUILD_EXAMPLES=OFF \
+		  -D BUILD_SHARED_LIBS=OFF \
+          -D OPENCV_EXTRA_MODULES_PATH="${WD}/opencv_contrib/modules/"  \
           ../..
     # Build it
     make -j${N_JOBS}
